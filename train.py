c22e1183d1e63cffa3af89e9c181f3595cca4a81
Marwan900 <marwan.elfakih@gmail.com>
2023-12-13 16:57:58 +0100

    Last Comit

--- train.py
+++ train.py
@@ -2,6 +2,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+
 
 # Charger les données nettoyées
 data = pd.read_csv('cleaned_train.csv')
@@ -10,17 +13,24 @@ data = pd.read_csv('cleaned_train.csv')
 X = data.drop('Survived', axis=1)
 y = data['Survived']
 
+
 # Division en ensembles d'entraînement et de test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Construction et entraînement du modèle (Régression logistique)
-model = LogisticRegression(random_state=42)
-model.fit(X_train, y_train)
+# Normalisation des données
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Construction et entraînement du modèle
+model = RandomForestClassifier()
+model.fit(X_train_scaled, y_train)
 
 # Évaluation du modèle sur l'ensemble de test
 predictions = model.predict(X_test)
 
-# Évaluation de la performance
+# Évaluation du modèle
+predictions = model.predict(X_test_scaled)
 accuracy = accuracy_score(y_test, predictions)
 precision = precision_score(y_test, predictions)
 recall = recall_score(y_test, predictions)
@@ -30,7 +40,6 @@ print(f'Accuracy: {accuracy}')
 print(f'Precision: {precision}')
 print(f'Recall: {recall}')
 print(f'F1 Score: {f1}')
-
 # Cross Validation
 cv_scores = cross_val_score(model, X, y, cv=5)
 average_cv_score = cv_scores.mean()
